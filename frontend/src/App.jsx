1e3096723f8db5babf662f5cef35059555f0d22d
prajjwallamsal202@gmail.com <Prajjwal@123>
2026-01-13 11:53:52 +0545

    feat: Add coupon route and navigation to admin panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,6 +56,7 @@ function App() {
           <Route path="subscriptions" element={<SubscriptionManagement />} />
           <Route path="transactions" element={<TransactionManagement />} />
           <Route path="customers" element={<CustomerManagement />} />
+          <Route path="coupons" element={<CouponManagement />} />
           <Route path="tickets" element={<TicketManagement />} />
           <Route path="faqs" element={<FAQManagement />} />
           <Route path="tutorials" element={<TutorialManagement />} />
